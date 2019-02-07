d9d84da164b6db03e34df8e7fc1488cf27537606
Jake Bunce <jake@omise.co>
2019-02-07 14:10:43 +0700

    [plasma-cli] Initial commit of plasma_cli.go

---
+++ plasma_cli.go
@@ -0,0 +1,369 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"crypto/ecdsa"
+	//"crypto/rand"
+	"encoding/json"
+	//"encoding/hex"
+	"fmt"
+	log "github.com/sirupsen/logrus"
+	"os"
+	"io/ioutil"
+	"math/big"
+	"net/http"
+	"strings"
+	"strconv"
+
+	"rootchain"
+
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/ethereum/go-ethereum/rlp"
+	"gopkg.in/alecthomas/kingpin.v2"
+)
+
+var (
+	get = kingpin.Command("get", "Get a resource.").Default()
+	getUTXO = get.Command("utxos", "Retrieve UTXO data from the Watcher service")
+	watcherURL = get.Flag("watcher", "FQDN of the Watcher in the format http://watcher.path.net").Required().String()
+	ownerUTXOAddress = get.Flag("address", "Owner address to search UTXOs").String()
+	status = get.Command("status", "Get status from the Watcher")
+	deposit = kingpin.Command("deposit", "Deposit ETH or ERC20 into the Plamsa MoreVP smart contract.")
+	privateKey = deposit.Flag("privatekey", "Private key of the account used to send funds into Plasma MoreVP").Required().String()
+	client = deposit.Flag("client", "Address of the Ethereum client. Infura and local node supported https://rinkeby.infura.io/v3/api_key or http://localhost:8545").Required().String()
+	contract = deposit.Flag("contract", "Address of the Plasma MoreVP smart contract").Required().String()
+	transaction = kingpin.Command("transaction", "Create a transaction on the OmiseGO Plasma MoreVP network")
+	blknum = transaction.Flag("block", "Block number").Required().String()
+	txindex = transaction.Flag("txindex", "Transaction Index").Required().String()
+	oindex = transaction.Flag("oindex", "Output Index").Required().String()
+	cur12 = transaction.Flag("currency", "Currency of the transaction").Required().String()
+	newowner = transaction.Flag("newowner", "New owner of the UTXO").Required().String()
+	amount = transaction.Flag("amount", "Amount to transact").Required().String()
+	signing = transaction.Flag("key", "Private key for signing the transaction").Required().String()
+)
+
+type plasmaTransaction struct {
+	blknum int
+	txindex int
+	oindex int
+	cur12 string
+	newowner string
+	amount float64
+}
+
+type plasmaDeposit struct {
+	privateKey string
+	client string
+	contract string
+}
+
+type watcherStatus struct {
+	Version string `json:"version"`
+	Success bool   `json:"success"`
+	Data    struct {
+		LastValidatedChildBlockNumber int           `json:"last_validated_child_block_number"`
+		LastMinedChildBlockTimestamp  int           `json:"last_mined_child_block_timestamp"`
+		LastMinedChildBlockNumber     int           `json:"last_mined_child_block_number"`
+		EthSyncing                    bool          `json:"eth_syncing"`
+		ByzantineEvents               []interface{} `json:"byzantine_events"`
+	} `json:"data"`
+}
+
+type watcherUTXOsFromAddress struct {
+	Version string `json:"version"`
+	Success bool   `json:"success"`
+	Data    []struct {
+		UtxoPos  int    `json:"utxo_pos"`
+		Txindex  int    `json:"txindex"`
+		Owner    string `json:"owner"`
+		Oindex   int    `json:"oindex"`
+		Currency string `json:"currency"`
+		Blknum   int    `json:"blknum"`
+		Amount   int    `json:"amount"`
+	} `json:"data"`
+}
+
+type watcherError struct {
+	Version string `json:"version"`
+	Success bool   `json:"success"`
+	Data    struct {
+		Object      string `json:"object"`
+		Description string `json:"description"`
+		Code        string `json:"code"`
+	} `json:"data"`
+}
+
+type ownerUTXOs struct {
+	Txindex  int    `json:"txindex"`
+	Txbytes  string `json:"txbytes"`
+	Oindex   int    `json:"oindex"`
+	Currency string `json:"currency"`
+	Blknum   int    `json:"blknum"`
+	Amount   int    `json:"amount"`
+}
+
+type transactionSuccessResponse struct {
+	Version string `json:"version"`
+	Success bool   `json:"success"`
+	Data    struct {
+		Blknum  int    `json:"blknum"`
+		Txindex int    `json:"txindex"`
+		Txhash  string `json:"txhash"`
+	} `json:"data"`
+}
+
+type transactionFailureResponse struct {
+	Version string `json:"version"`
+	Success bool   `json:"success"`
+	Data    struct {
+		Object      string `json:"object"`
+		Code        string `json:"code"`
+		Description string `json:"description"`
+		Messages    struct {
+			ErrorKey string `json:"error_key"`
+		} `json:"messages"`
+	} `json:"data"`
+}
+
+type blockNumber struct {
+	Hash string `json:"hash"`
+}
+
+type blockNumberError struct {
+	Version string `json:"version"`
+	Success bool   `json:"success"`
+	Data    struct {
+		Object   string `json:"object"`
+		Messages struct {
+			ValidationError struct {
+				Validator string `json:"validator"`
+				Parameter string `json:"parameter"`
+			} `json:"validation_error"`
+		} `json:"messages"`
+		Description string `json:"description"`
+		Code        string `json:"code"`
+	} `json:"data"`
+}
+
+// Add the full time include timezone into log messages
+// INFO[2019-01-31T16:38:57+07:00]
+func logFormatter() {
+	formatter := &log.TextFormatter{
+		FullTimestamp: true,
+	}
+	log.SetFormatter(formatter)
+}
+
+//Retrieve the UTXOs associated with an address from the Watcher
+func getUTXOsFromAddress(address string, w string) watcherUTXOsFromAddress {
+	// Build request
+	var url strings.Builder
+	url.WriteString(w)
+	url.WriteString("/account.get_utxos")
+	postData := map[string]interface{}{"address": address, "limit": "10000"}
+	js, _ := json.Marshal(postData)
+	r, err := http.NewRequest("POST", url.String(), bytes.NewBuffer(js))
+	if err != nil {
+		log.Fatal(err)
+	}
+	r.Header.Add("Content-Type", "application/json")
+
+	// Make the request
+	client := &http.Client{}
+	resp, err := client.Do(r)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	// Unmarshall the response
+	response := watcherUTXOsFromAddress{}
+
+	rstring, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	jsonErr := json.Unmarshal([]byte(rstring), &response)
+	if jsonErr != nil {
+		log.Warning("Could not unmarshal successful response from the Watcher")
+		errorInfo := watcherError{}
+		processError := json.Unmarshal([]byte(rstring), &errorInfo)
+		if processError != nil { // Response from the Watcher does not match a struct
+			log.Fatal("Unknown response from Watcher API")
+			panic("uh oh")
+		}
+		log.Warning("Unmarshalled JSON error response from the Watcher API")
+		log.Error(errorInfo)
+	} else {
+		log.Info(resp.Status)
+	}
+
+	return response
+}
+
+//Deposit ETH into the already deployed Plasma MoreVP contract on Ethereum
+func (d *plasmaDeposit) depositToPlasmaContract() {
+	client, err := ethclient.Dial(d.client)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	privateKey, err := crypto.HexToECDSA(d.privateKey)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	publicKey := privateKey.Public()
+	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
+	if !ok {
+		log.Fatal("Cannot assert type: publicKey is not of type *ecdsa.PublicKey")
+	}
+
+	fromAddress := crypto.PubkeyToAddress(*publicKeyECDSA)
+	nonce, err := client.PendingNonceAt(context.Background(), fromAddress)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	gasPrice, err := client.SuggestGasPrice(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+	auth := bind.NewKeyedTransactor(privateKey)
+	auth.Nonce = big.NewInt(int64(nonce))
+	auth.Value = big.NewInt(100)   // in wei
+	auth.GasLimit = uint64(210000) // in units
+	auth.GasPrice = gasPrice
+
+	address := common.HexToAddress(d.contract)
+	owner := "\x94J\x81\xbe\xec\xac\x91\x80'\x87\xfb\xcf\xb9v\x7f\xcb\xf8\x1d\xb1\xf5"
+	rlpInputs := []byte("\xf8\xc3\xd0\xc3\x80\x80\x80\xc3\x80\x80\x80\xc3\x80\x80\x80\xc3\x80\x80\x80\xf8\xb0\xeb\x94 <owner_hex> \x94\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00d\xeb\x94\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x94\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x80\xeb\x94\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x94\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x80\xeb\x94\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x94\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x80")
+	instance, err := rootchain.NewRootchain(address, client)
+	if err != nil {
+		log.Fatal(err)
+	}
+	t := &bind.TransactOpts{}
+	t.From = fromAddress
+	t.Signer = auth.Signer
+	t.Value = big.NewInt(100)
+	tx, err := instance.Deposit(t, rlpInputs)
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		log.Info("Deposit to Plasma MoreVP contract sent. Transaction: ", tx.Hash().Hex())
+	}
+}
+
+func getWatcherStatus(w string) {
+	var url strings.Builder
+	url.WriteString(w)
+	url.WriteString("/status.get")
+	r, err := http.NewRequest("POST", url.String(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	r.Header.Add("Content-Type", "application/json")
+
+	// Make the request
+	client := &http.Client{}
+	resp, err := client.Do(r)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	// Unmarshall the response
+	response := watcherStatus{}
+
+	rstring, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	jsonErr := json.Unmarshal([]byte(rstring), &response)
+	if jsonErr != nil {
+		log.Error(jsonErr)
+	}
+	log.Info("Last validated Childchain block number: ", response.Data.LastValidatedChildBlockNumber)
+	log.Info("Last mined Childchain block number: ", response.Data.LastMinedChildBlockNumber)
+}
+
+// Displays the UTXO ownership data in a human friendly format
+func (u *watcherUTXOsFromAddress) displayUTXOS() {
+		for _, value := range u.Data {
+			log.Info("Owner: ", value.Owner)
+			log.Info("Amount: ", value.Amount)
+			log.Info("Block Number: ", value.Blknum)
+			log.Info("Transaction Index: ", value.Txindex)
+			log.Info("Output Index: ", value.Oindex)
+			log.Info("Currency: ", value.Currency)
+		}
+}
+
+// Create a transaction from the user input from UTXO data.
+// This needs RLP encoding and then signing with the private
+// key of the sending party.
+func (t *plasmaTransaction) createPlasmaTransaction(privateKey string) {
+	// RLP encode the transaction
+	transaction := *t
+	rlpInputs, err := rlp.EncodeToBytes(transaction)
+	if err != nil {
+		log.Fatal("Error encoding RLP")
+	} else {
+		log.Info("Transaction data structure RLP encoded")
+	}
+	fmt.Printf("%v", rlpInputs)
+    //rlpDecoded := rlp.DecodeBytes(rlpInputs, os.Stdout)
+	//log.Printf("%v", rlpDecoded)
+	//r, s, serr := ecdsa.Sign(rand.Reader, privateKey, hash)
+}
+
+func convertStringToInt(value string) int {
+	i, err := strconv.Atoi(value)
+	if err != nil {
+		log.Fatal("Could not convert value")
+	}
+	return i
+}
+
+func convertStringToFloat64(value string) float64 {
+	f, err := strconv.ParseFloat(value, 64)
+	if err != nil {
+		log.Fatal("Could not convert value")
+	}
+	return f
+}
+
+func main() {
+	logFormatter()
+	log.Info("Starting OmiseGO Plasma MoreVP CLI")
+	switch kingpin.Parse() {
+	case getUTXO.FullCommand():
+		//plasma_cli get utxos --address=0x944A81BeECac91802787fBCFB9767FCBf81db1f5 --watcher=http://watcher.path.net
+		ownerUTXOAddress := *ownerUTXOAddress
+		//address := []byte{"0x944A81BeECac91802787fBCFB9767FCBf81db1f5"}
+		//a := hex.EncodeToString([]byte("0x944A81BeECac91802787fBCFB9767FCBf81db1f5"))
+		//println(a)
+		if len(ownerUTXOAddress) == 0 {
+			log.Error("Address is required to get UTXO data")
+			os.Exit(1)
+		}
+		utxos := getUTXOsFromAddress(ownerUTXOAddress, *watcherURL)
+		utxos.displayUTXOS()
+	case status.FullCommand():
+		//plamsa_cli get status --watcher=http://watcher.path.net
+		getWatcherStatus(*watcherURL)
+	case deposit.FullCommand():
+		//plasma_cli deposit --privatekey=0x944A81BeECac91802787fBCFB9767FCBf81db1f5 --client=https://rinkeby.infura.io/v3/api_key --contract=0x457e2ec4ad356d3cb449e3bd4ba640d720c30377
+		d := plasmaDeposit{privateKey: *privateKey, client: *client, contract: *contract}
+		d.depositToPlasmaContract()
+	case transaction.FullCommand():
+		//plasma_cli transaction --block --txindex --oindex --currency --newowner --amount
+		t := plasmaTransaction{blknum: convertStringToInt(*blknum), txindex: convertStringToInt(*txindex), oindex: convertStringToInt(*oindex), cur12: *cur12, newowner: *newowner, amount: convertStringToFloat64(*amount)}
+		t.createPlasmaTransaction(*signing)
+	}
+}
